2c7caabec6448364ef33a33a7c761c2dc21282cc
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-15 17:15:47 +0700

    feat(helper): add join url func

--- helper/utils.go
+++ helper/utils.go
@@ -1,5 +1,21 @@
 package helper
 
-func JoinURL(baseURL, path string) (url string, err error) {
+import (
+	"net/url"
+)
+
+func JoinURL(baseUrlStr, pathUrlStr string) (joinedUrlStr string, err error) {
+	baseUrl, err := url.Parse(baseUrlStr)
+	if err != nil {
+		return
+	}
+
+	pathUrl, err := url.Parse(pathUrlStr)
+	if err != nil {
+		return
+	}
+
+	joinedUrlStr = baseUrl.ResolveReference(pathUrl).String()
+
 	return
 }
