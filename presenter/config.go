0f4c4f162dedb2e483d3e94f5a97e571dcd557f8
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-17 02:05:00 +0700

    fix(presenter): add json tag to struct

--- presenter/config.go
+++ presenter/config.go
@@ -2,9 +2,11 @@ package presenter
 
 type (
 	Config struct {
-		DigitalCurrency struct {
-			ServerAddress string
-			ApiKey        string
-		}
+		DigitalCurrency DigitalCurrency `json:"digital_currency"`
+	}
+
+	DigitalCurrency struct {
+		ServerAddress string `json:"server_address"`
+		ApiKey        string `json:"api_key"`
 	}
 )
