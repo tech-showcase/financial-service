99ae60546838e3049939b9de2686bd2dbce29115
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-17 01:53:58 +0700

    teat(config): add test for parse config func

--- config/config_test.go
+++ config/config_test.go
@@ -1,9 +1,34 @@
 package config
 
 import (
+	"fmt"
+	"github.com/tech-showcase/financial-service/presenter"
 	"os"
+	"reflect"
 	"testing"
 )
+
+func TestParse(t *testing.T) {
+	expectedOutput := presenter.Config{
+		DigitalCurrency: presenter.DigitalCurrency{
+			ServerAddress: "http://dummy.address/",
+			ApiKey:        "dummy-key",
+		},
+	}
+
+	configPath := "config.json"
+	os.Setenv("DEV_CONFIG_PATH", configPath)
+
+	config, err := Parse()
+
+	fmt.Println(config)
+
+	if err != nil {
+		t.Fatal("an error has occurred")
+	} else if !reflect.DeepEqual(config, expectedOutput) {
+		t.Fatal("unexpected output")
+	}
+}
 
 func TestGetPath(t *testing.T) {
 	expectedOutput := "config/config-prod.json"
