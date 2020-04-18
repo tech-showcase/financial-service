b8b42d6cb8e276282d1e507554442a94fb2050cf
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-18 23:12:49 +0700

    feat(config): add auth configuration

--- presenter/config.go
+++ presenter/config.go
@@ -3,10 +3,15 @@ package presenter
 type (
 	Config struct {
 		DigitalCurrency DigitalCurrency `json:"digital_currency"`
+		Auth            Auth            `json:"auth"`
 	}
 
 	DigitalCurrency struct {
 		ServerAddress string `json:"server_address"`
 		ApiKey        string `json:"api_key"`
+	}
+
+	Auth struct {
+		ServerAddress string `json:"server_address"`
 	}
 )
