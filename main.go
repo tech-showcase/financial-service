2922c8bf7c600034c1b5c6acb4660bb90b93ba14
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-19 02:30:47 +0700

    feat(main): print some string to mark app starts

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/tech-showcase/financial-service/api"
 	"github.com/tech-showcase/financial-service/cmd"
 )
 
 func main() {
+	fmt.Println("Hi, I am Financial Service!")
+
 	args := cmd.Parse()
 
 	api.Activate(args.Port)
