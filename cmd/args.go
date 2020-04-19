a94a94d55f30f8dfb4a07f0c1ea5c9a8607dbb2d
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-19 23:31:01 +0700

    feat(cmd): change default port

--- cmd/args.go
+++ cmd/args.go
@@ -6,7 +6,7 @@ import (
 )
 
 func Parse() (args presenter.Args) {
-	flag.IntVar(&args.Port, "port", 8080, "Port which apps will listen to")
+	flag.IntVar(&args.Port, "port", 8082, "Port which service will listen to")
 	flag.Parse()
 
 	return
