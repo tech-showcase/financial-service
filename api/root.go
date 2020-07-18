f12fd558a7639a43ebe6add954865eeed7bfb565
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-07-19 03:57:28 +0700

    refactor(route): remove redundancy

--- api/root.go
+++ api/root.go
@@ -1,5 +1,30 @@
 package api
 
+import (
+	"fmt"
+	grpcMiddleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	"github.com/tech-showcase/financial-service/middleware"
+	"github.com/tech-showcase/financial-service/presenter"
+	digitalCurrencyProto "github.com/tech-showcase/financial-service/proto/digitalcurrency"
+	"google.golang.org/grpc"
+	"net"
+)
+
 func Activate(port int) {
-	ActivateGRPC(port)
+	lisAddress := fmt.Sprintf(":%d", port)
+
+	lis, err := net.Listen("tcp", lisAddress)
+	if err != nil {
+		panic(err)
+	}
+
+	grpcServer := grpc.NewServer(withInterceptor())
+	digitalCurrencyProto.RegisterDigitalCurrencyServer(grpcServer, presenter.NewDigitalCurrencyServer())
+	grpcServer.Serve(lis)
+}
+
+func withInterceptor() grpc.ServerOption {
+	return grpc.UnaryInterceptor(grpcMiddleware.ChainUnaryServer(
+		middleware.LoggingInterceptor,
+		middleware.AuthorizationInterceptor))
 }
