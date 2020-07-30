e193f6819c1fb169e013cd16fd73a8d95649f404
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-07-31 04:14:16 +0700

    feat(helper): add oauth2 helper to authorize and get user token

---
+++ helper/oauth2.go
@@ -0,0 +1,70 @@
+package helper
+
+import (
+	"context"
+	"golang.org/x/oauth2"
+	"time"
+)
+
+type (
+	Token struct {
+		AccessToken  string    `json:"access_token"`
+		TokenType    string    `json:"token_type,omitempty"`
+		RefreshToken string    `json:"refresh_token,omitempty"`
+		Expiry       time.Time `json:"expiry,omitempty"`
+	}
+	oauth2Helper struct{}
+	OAuth2Helper interface {
+		HandleAuthorizeRequest() (result string)
+		HandleTokenRequest(authorizationCode string) (token Token, err error)
+	}
+)
+
+var (
+	oauth2Config = oauth2.Config{
+		ClientID:     "082222333444",
+		ClientSecret: "082222333444",
+		Scopes:       []string{"all"},
+		RedirectURL:  "http://localhost:8082/oauth2/token",
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  authServerURL + "/authorize",
+			TokenURL: authServerURL + "/token",
+		},
+	}
+
+	OAuth2HelperInstance OAuth2Helper
+)
+
+const (
+	authServerURL = "http://localhost:8080/oauth2"
+)
+
+func init() {
+	OAuth2HelperInstance = NewOAuth2Helper()
+}
+
+func NewOAuth2Helper() OAuth2Helper {
+	var instance oauth2Helper
+
+	return &instance
+}
+
+func (instance *oauth2Helper) HandleAuthorizeRequest() (result string) {
+	result = oauth2Config.AuthCodeURL("xyz")
+	return
+}
+
+func (instance *oauth2Helper) HandleTokenRequest(authorizationCode string) (token Token, err error) {
+	oAuth2Token, err := oauth2Config.Exchange(context.Background(), authorizationCode)
+	if err != nil {
+		return
+	}
+
+	token = Token{
+		AccessToken:  oAuth2Token.AccessToken,
+		TokenType:    oAuth2Token.TokenType,
+		RefreshToken: oAuth2Token.RefreshToken,
+		Expiry:       oAuth2Token.Expiry,
+	}
+	return
+}
