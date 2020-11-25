a1763c5d5764ff0dca7546a932d6d8ba3475a00d
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-11-25 16:25:55 +0700

    chore: update docker base image

--- tool/tool.go
+++ tool/tool.go
@@ -3,8 +3,7 @@
 package tool
 
 import (
+	_ "github.com/golang/protobuf/protoc-gen-go"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
-	_ "github.com/golang/protobuf/protoc-gen-go"
 )
-
